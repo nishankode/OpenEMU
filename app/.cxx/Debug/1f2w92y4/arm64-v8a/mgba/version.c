f6a8e93281c45f0e81b683397cef373f4f849cc8
nishan0000 <mdnishan006@gmail.com>
2026-05-17 23:24:13 +0530

    fix:Reposition local link controls below game screen

--- app/.cxx/Debug/1f2w92y4/arm64-v8a/mgba/version.c
+++ app/.cxx/Debug/1f2w92y4/arm64-v8a/mgba/version.c
@@ -5,10 +5,10 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 #include <mgba/core/version.h>
 
-MGBA_EXPORT const char* const gitCommit = "2a1c3a4b2638c2f034085069e3c21241720f8f65-dirty";
-MGBA_EXPORT const char* const gitCommitShort = "2a1c3a4-dirty";
+MGBA_EXPORT const char* const gitCommit = "2c02b12d561937415a98bff89ac67ff376981cd5-dirty";
+MGBA_EXPORT const char* const gitCommitShort = "2c02b12-dirty";
 MGBA_EXPORT const char* const gitBranch = "main";
-MGBA_EXPORT const int gitRevision = 21;
+MGBA_EXPORT const int gitRevision = 22;
 MGBA_EXPORT const char* const binaryName = "linkroom_mgba";
 MGBA_EXPORT const char* const projectName = "mGBA";
-MGBA_EXPORT const char* const projectVersion = "0.10-main-21-2a1c3a4-dirty";
+MGBA_EXPORT const char* const projectVersion = "0.10-main-22-2c02b12-dirty";
